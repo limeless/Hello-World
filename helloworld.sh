4f9ce201b15ad22c40e2518ad19fe9b199cb2f81
Ben Wang <limelight.wang@gmail.com>
2012-03-05 10:54:05 +0800

    this is simple helloworld about sh script

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Program:
+#   This program shows "HelloWorld" On Screen
+#History:
+#   2011/12/24 First Release
+#       Limelight.Wang@gmail.com
+
+echo "Hello World! \a\n"
+exit 1
+
+if exit == 1 
+    echo"Thi is a test script if exit command send 0"
+    exit 0 
